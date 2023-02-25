ecc0a5873d5c8d7f40d75524c536eef5cd6c5c53
moahasni00 <126292548+moahasni00@users.noreply.github.com>
2023-02-25 01:21:35 +0100

    added new files01

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Green Binary Code Background</title>
+	<style>
+		body {
+			background-color: #0f0;
+			background-image: url("data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7");
+			color: #fff;
+			font-size: 48px;
+			text-align: center;
+			margin: 0;
+			padding: 0;
+		}
+	</style>
+</head>
+<body>
+	<div>
+		HAHAHA TQOLEBTI SALMA TELEFONEK THACKA HAHAHAHA
+	</div>
+</body>
+</html>
